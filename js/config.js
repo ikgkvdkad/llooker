bac27a2cfcab566f8a8b95afa1ecc59b7492e979
ikgkvdkad <ikgkvdkad@gmail.com>
2025-11-12 14:37:53 -0300

    Bump app version to V16.0

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Application configuration and constants
 
-export const APP_VERSION = 15.0;
+export const APP_VERSION = 16.0;
 
 export const DEFAULT_BACK_ASPECT = 16 / 9;
 export const DEFAULT_SELFIE_ASPECT = 16 / 9;
