cc2172b3e11d685e40f8981c29e8ee84831f8c69
ikgkvdkad <ikgkvdkad@gmail.com>
2025-11-15 19:41:59 -0300

    Bump version to 34.0

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Application configuration and constants
 
-export const APP_VERSION = 33.0;
+export const APP_VERSION = 34.0;
 
 export const DEFAULT_BACK_ASPECT = 16 / 9;
 export const DEFAULT_SELFIE_ASPECT = 16 / 9;
